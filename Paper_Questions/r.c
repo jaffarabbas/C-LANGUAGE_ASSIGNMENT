b895420d30f042be6da761c4debbdcf364b46fd4
Jaffar Abbas <gamaportal8@gmail.com>
2020-08-28 21:23:44 +0500

    28-8-2020

---
+++ Paper_Questions/r.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+//done by DARKLORD.
+//BELONGING ORGANIZATION : MAJU.@APTECH.@PIAIC.@WORLD_OF_DARKLORD_
+//Main_Source git link : https://github.com/jaffarabbas
+//Site:https://assignment-69ea5.web.app/
+//Mail:gamaportal8@gmail.com
+//GIT LINK : https://github.com/jaffarabbas/C-LANGUAGE_ASSIGNMENT.git
+//Write a program to find the factorial value of any number entered through the keyboard
+void work();
+
+int main(){
+	work();
+}
+
+void work(){
+	int n,i,f;
+    f=i=1;
+    printf("Enter a Number to Find Factorial: ");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+        f*=i;
+        i++;
+    }
+    printf("The Factorial of %d is : %d",n,f);
+    getch();
+}
